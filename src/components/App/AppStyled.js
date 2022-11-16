926364e080a52d60a92d03f1d45a85410509eab2
color <95304946+HermanMazur@users.noreply.github.com>
2022-11-16 14:28:53 +0200

    Update AppStyled.js

--- src/components/App/AppStyled.js
+++ src/components/App/AppStyled.js
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   padding: 15px 30px;
-  margin-right: auto;
-  margin-left: auto;
+  // margin-right: auto;
+  // margin-left: auto;
   border-radius: 15px;
   width: 450px;
-box-shadow: 0 1px 4px rgba(0, 0, 0, 1), -23px 0 20px -23px rgba(0, 0, 0, 0.8),
-            23px 0 20px -23px rgba(0, 0, 0, 0.8), 0 0 40px rgba(0, 0, 0, 0.1) inset;
+// box-shadow: 0 1px 4px rgba(0, 0, 0, 1), -23px 0 20px -23px rgba(0, 0, 0, 0.8),
+//             23px 0 20px -23px rgba(0, 0, 0, 0.8), 0 0 40px rgba(0, 0, 0, 0.1) inset;
  
 `;
 
